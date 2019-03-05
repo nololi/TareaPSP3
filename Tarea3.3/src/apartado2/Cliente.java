01cd5f56a94c25d424a239813e5621dcf2f5de7b
ue57656 <ue57656@>
2019-03-05 09:38:32 +0100

    Modificaciones varias

--- Tarea3.3/src/apartado2/Cliente.java
+++ Tarea3.3/src/apartado2/Cliente.java
@@ -14,42 +14,45 @@ import java.net.Socket;
  */
 public class Cliente {
 
-	public Cliente() {
+	public Cliente() { // constructor cliente
 		Socket cliente = null;
 		DataInputStream dataInputStream = null;
 		DataOutputStream dataOutputStream = null;
 		BufferedReader entrada = null;
 		try {
-			cliente = new Socket("localhost", 1500);
-			dataOutputStream = new DataOutputStream(cliente.getOutputStream());// para mandar al servidor
-			dataInputStream = new DataInputStream(cliente.getInputStream());// para lo que devuelve el servidor
-			entrada = new BufferedReader(new InputStreamReader(System.in)); // para la entrada del datos del usuario
+			cliente = new Socket("localhost", 2000); // socket cliente en puerto 2000
+			dataOutputStream = new DataOutputStream(cliente.getOutputStream());// stream para mandar al servidor
+			dataInputStream = new DataInputStream(cliente.getInputStream());// stream para lo que devuelve el servidor
+			entrada = new BufferedReader(new InputStreamReader(System.in)); // buffer para la entrada del datos del
+																			// usuario
 		} catch (IOException e) {
 			System.out.println("No se ha podido crear el socket cliente" + e.getMessage());
 			System.exit(1);
 		}
 
-		// pedir usuario
-		String usuario = pedirDato(entrada, "usuario");
-		// pedir contraseña
-		String contra = pedirDato(entrada, "contraseña");
-
-		// envío y recibo el directorio
-		String serverResponse = enviarYRecibirServidor(usuario + "-" + contra, dataOutputStream, dataInputStream);
-		if (serverResponse.contains("Error")) {
-			System.out.println(serverResponse);
-		} else {
-			// Si el usuario y contraseña son correctos
-			System.out.println(serverResponse);
-			System.out.println("Introduzca nombre del fichero");
-			// pedir nombre del fichero al usuario
-			String fichero = pedirDato(entrada, "fichero");
-			// enviar nombre del fichero al servidor y tratar la respuesta
-			serverResponse = enviarYRecibirServidor(fichero, dataOutputStream, dataInputStream);
-
-			System.out.println(serverResponse == null ? "Error, ha solicitado un directorio" : serverResponse);
-
+		String serverResponse = ""; // variable para las respuestas del servidor
+		while (true) {
+			String usuario = pedirDato(entrada, "usuario");// pedir usuario
+			String contra = pedirDato(entrada, "contraseña");// pedir contraseña
+			
+			// envío y recibo el directorio
+			serverResponse = enviarYRecibirServidor(usuario + "-" + contra, dataOutputStream, dataInputStream);
+			if (!serverResponse.contains("Error")) { // si la respuesta del cliente no indica Error, continúo
+				System.out.println(serverResponse);
+				break;
+			}
+			
+			System.out.println(serverResponse); // muestro el mensaje de error
 		}
+
+		// Si el usuario y contraseña son correctos pido el nombre del fichero
+		System.out.println("Introduzca nombre del fichero");
+		String fichero = pedirDato(entrada, "fichero");
+		
+		// enviar nombre del fichero al servidor y tratar la respuesta
+		serverResponse = enviarYRecibirServidor(fichero, dataOutputStream, dataInputStream);
+		System.out.println(serverResponse==null ?"" : serverResponse); //muestro contenido fichero si no es null
+		
 		// cerrar las conexiones, streams...
 		cerrarConexion(cliente, dataOutputStream, dataInputStream);
 	}
@@ -79,8 +82,9 @@ public class Cliente {
 	 */
 	public String enviarYRecibirServidor(String fichero, DataOutputStream dataOutputStream,
 			DataInputStream dataInputStream) {
+		// enviar datos
 		try {
-			dataOutputStream.writeUTF(fichero);// enviar datos
+			dataOutputStream.writeUTF(fichero);
 		} catch (IOException e1) {
 			System.out.println("Error al enviar al servidor" + e1.getMessage());
 		}
@@ -89,13 +93,15 @@ public class Cliente {
 			// recibir datos
 			return dataInputStream.readUTF();
 		} catch (IOException e) {
-			System.out.println("Error al recibir del servidor" + (e.getMessage() == null ? "" : e.getMessage()));
+			// si el servidor devuelve null es posible que haya solicitado un directorio
+			System.out.println("Error al recibir del servidor :"
+					+ (e.getMessage() == null ? " Error, ¿ha solicitado un directorio?" : e.getMessage()));
 		}
 		return null;
 	}
 
 	/*
-	 * Con este método cerramos buffers y el socket
+	 * Con este método cerramos streams y el socket
 	 */
 	public void cerrarConexion(Socket c, DataOutputStream dataOutputStream, DataInputStream dataInputStream) {
 		if (c != null) {
--- Tarea3.3/src/apartado2/Servidor.java
+++ Tarea3.3/src/apartado2/Servidor.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author N
@@ -18,16 +17,17 @@ import java.util.List;
 public class Servidor extends Thread {
 	Socket socket;
 
-	private static final int PUERTO = 1500;
-	private static int NUMERO_CLIENTE = 1;
-	private static final File DIRECTORY = new File(System.getProperty("user.dir"));
-	private static final String[] LISTADO_FICHEROS_TEXTO = DIRECTORY.list();
-	private static final File[] LISTADO_TIPO_FICHERO = DIRECTORY.listFiles();
-	private final String usuario = "Noelia";
-	private final String contra = "abc123";
-	private int miNumeroCliente;
+	private static final int PUERTO = 2000;// puerto utilizado
+	private static int NUMERO_CLIENTE = 1; // contador global de clientes
+	private static final File DIRECTORY = new File(System.getProperty("user.dir"));// directorio actual
+	private static final String[] LISTADO_NOMBRES_FICHERO = DIRECTORY.list();// listado nombre ficheros (String)
+	private static final File[] LISTADO_TIPO_FICHERO = DIRECTORY.listFiles();// listado ficheros como objetos File
 
-	public Servidor(Socket sCliente) {
+	private final String usuario = "Noelia";// usuario para el login
+	private final String contra = "abc123"; // contraseña para el login
+	private int miNumeroCliente = 1;// número del cliente que se ha conectado
+
+	public Servidor(Socket sCliente) {// constructor que inicializa el servidor con el socketcliente
 		socket = sCliente;
 	}
 
@@ -40,92 +40,102 @@ public class Servidor extends Thread {
 	}
 
 	public void run() {
+		// muestro mensaje, asigno número de cliente, y lo aumento en +1
 		System.out.println("Conexión creada correctamente número " + NUMERO_CLIENTE);
 		setMiNumeroCliente(NUMERO_CLIENTE);
 		NUMERO_CLIENTE++;
-		DataInputStream dataInputStream = null;
+
+		DataInputStream dataInputStream = null;// Stream de entrada
+		DataOutputStream dataOutputStream = null;// Stream de salida
+
 		try {
+			// inicializo streams de entrada y de salida
 			dataInputStream = new DataInputStream(socket.getInputStream());
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		DataOutputStream dataOutputStream = null;
-		try {
 			dataOutputStream = new DataOutputStream(socket.getOutputStream());
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
 
-		try {
-			dataOutputStream = new DataOutputStream(socket.getOutputStream());// para las salidas
-			dataInputStream = new DataInputStream(socket.getInputStream());// para las entradas
-
-			String data = dataInputStream.readUTF();
-			String nombre = data.split("-")[0];
-			String password = data.split("-")[1];
-			if (!nombre.equals(usuario) && !password.equals(contra)) {
-				mandarDatosCliente(dataOutputStream, "Error : Usuario y/o contraseña errónea");
-				System.out.println("Desconectado ");
-			} else {
-				// muestro directorio actual y envío los nombres del contenido
-				String directoryNameFiles = "";
-				String line = "";
-				for (int i = 0; i < LISTADO_FICHEROS_TEXTO.length; i++) {
-					String tipoFichero = LISTADO_TIPO_FICHERO[i].isDirectory() ? "Directorio :" : "Fichero :";
-					directoryNameFiles += line + tipoFichero + LISTADO_FICHEROS_TEXTO[i];
-					line = "\n";
-				}
-
-				mandarDatosCliente(dataOutputStream, directoryNameFiles);
-				// quedo a la espera de que me pida qué fichero mostrar
-				String respuesta = dataInputStream.readUTF();
-
-				// si el fichero que ha introducido no existe error
-				if (!directoryNameFiles.contains(respuesta)) {
-					mandarDatosCliente(dataOutputStream, "El fichero solicitado no existe");
+			// espero a que me manden el usuario y contraseña correcto antes de continuar,
+			while (true) {
+				String data = dataInputStream.readUTF();
+				String nombre = data.split("-")[0];
+				String password = data.split("-")[1];
+				boolean loginOK = nombre.equals(usuario) && password.equals(contra);
+				if (!loginOK) {
+					mandarDatosCliente(dataOutputStream, "Error : Usuario y/o contraseña errónea");
 				} else {
-					File ficheroEncontrado = buscarFichero(respuesta);
-					buscarYMandarFichero(ficheroEncontrado, dataOutputStream);
+					break; // contraseña ok :salir
 				}
+			}
+
+			// recojo directorio actual por tipo (otro directorio/fichero) y nombre y lo
+			// almaceno en directoryNameFiles
+			String directoryNameFiles = "";
+			String line = "";
+			for (int i = 0; i < LISTADO_NOMBRES_FICHERO.length; i++) {
+				String tipoFichero = LISTADO_TIPO_FICHERO[i].isDirectory() ? "Directorio :" : "Fichero :";
+				directoryNameFiles += line + tipoFichero + LISTADO_NOMBRES_FICHERO[i];
+				line = "\n";
+			}
+
+			// mando el contenido del directorio y quedo a la espera de que me pida qué
+			// fichero mostrar
+			mandarDatosCliente(dataOutputStream, directoryNameFiles);
+			String ficheroSolicitado = dataInputStream.readUTF(); // fichero solicitado
+
+			if (!directoryNameFiles.contains(ficheroSolicitado)) {// si el fichero que ha introducido no existe error
+				mandarDatosCliente(dataOutputStream, "El fichero solicitado no existe");
+			} else { // si existe busco fichero y lo mando
+				File ficheroEncontrado = buscarFichero(ficheroSolicitado);
+				buscarYMandarFichero(ficheroEncontrado, dataOutputStream);
 			}
 
 		} catch (IOException e) {
 			System.out.println("No se ha podido crear el serverSocket" + e.getMessage());
 			System.exit(1);
+		} finally {
+			// cerramos conexiones al finalizar
+			cerrarConexion(socket, dataOutputStream, dataInputStream);
+			System.out.println("Cliente " + getMiNumeroCliente() + " desconectado");
 		}
 
-		cerrarConexion(socket, dataOutputStream, dataInputStream);
-		System.out.println("Cliente " + getMiNumeroCliente() + " desconectado");
 	}
 
 	/*
-	 * 
+	 * método que busca el fichero solicitado por el cliente y envía su contenido
 	 */
 	public void buscarYMandarFichero(File ficheroEncontrado, DataOutputStream dataOutputStream) {
-		if (ficheroEncontrado != null) {// si se encontró se envía
+		if (ficheroEncontrado != null) {// si se encontró un fichero con ese nombre se envía
 			FileReader fileReader = null;
+			BufferedReader br = null;
 			try {
 				fileReader = new FileReader(ficheroEncontrado);
-				BufferedReader br = new BufferedReader(fileReader);
-				String lineaFichero = "";
-				String datos = "";
+				br = new BufferedReader(fileReader);
+				String lineaFichero = ""; // cada línea leída del fichero
+				String datosParaCliente = ""; // datos que voy a mandar
 				String saltoLinea = "";
+				// almaceno el contenido del fichero en el String datosParaCliente
 				while ((lineaFichero = br.readLine()) != null) {
-					datos += saltoLinea + lineaFichero;
+					datosParaCliente += saltoLinea + lineaFichero;
 					saltoLinea = "\n";
 				}
-				dataOutputStream.writeUTF(datos);
-				if (br != null) {
-					br.close();
-				}
+				// envío los datos al cliente
+				mandarDatosCliente(dataOutputStream, datosParaCliente);
+
 			} catch (IOException e) {
 				System.out.println("Error al enviar el fichero al cliente" + e.getMessage());
+			} finally {
+				if (br != null) {
+					try {
+						br.close();
+					} catch (IOException e) {
+						System.out.println("Error al cerrar el bufferedReader");
+					}
+				}
 			}
 		} else {
-			try {
+			try {// si el fichero no se ha encontrado
 				dataOutputStream.writeUTF("No se ha encontrado el fichero");
 			} catch (IOException e) {
-				System.out.println("Error al enviar el fichero al cliente" + e.getMessage());
+				System.out.println("Error al contactar al cliente" + e.getMessage());
 			}
 		}
 	}
@@ -144,13 +154,13 @@ public class Servidor extends Thread {
 	}
 
 	/*
-	 * Envío de datos al cliente
+	 * Función genérica de envío de datos al cliente
 	 */
 	public void mandarDatosCliente(DataOutputStream dataOutputStream, String data) {
 		try {
 			dataOutputStream.writeUTF(data);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Error al contactar al cliente" + e.getMessage());
 			e.printStackTrace();
 		}
 	}
@@ -192,11 +202,14 @@ public class Servidor extends Thread {
 			while (true) {
 				socket = serverSocket.accept();// acepto la conexión con el cliente
 				new Servidor(socket).start();
+
 			}
 
 		} catch (IOException e) {
 			System.out.println("Error al conectarse" + e.getMessage());
 		} finally {
+			// cerramos los sockets
+			System.out.println("Finally");
 			if (serverSocket != null) {
 				try {
 					serverSocket.close();
